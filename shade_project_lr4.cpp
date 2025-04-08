00cbb1277c593b739162c79baf42f0955542c7f1
tatjana1231 <11bergsashka12@gmail.com>
2025-04-08 15:07:43 +0300

    create project

---
+++ shade_project_lr4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void addDigitRight(long long k, int d, long long& result){
+}
+
+void addDidigtLeft(long long k, int d, long long& result){
+}
+
+int main() {
+    long long k = 0;
+    int d = 0;
+    bool kEntered = false;
+    bool dEntered = false;
+    long long result; // Добавляем переменную result здесь
+
+    int choice;
+
+    do {
+        cout << "\nМеню:" << endl;
+        cout << "1. Ввести натуральное число K" << endl;
+        cout << "2. Ввести цифру D (в диапазоне 1-9)" << endl;
+        cout << "3. Добавить к числу K справа цифру D" << endl;
+        cout << "4. Добавить к числу K слева цифру D" << endl;
+        cout << "0. Выход" << endl;
+        cout << "Выберите действие: ";
+        cin >> choice;
+    } while (choice != 0);
+    return 0;
+}
